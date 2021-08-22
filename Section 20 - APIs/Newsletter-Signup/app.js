f076627e98c164e56d5fc92eda8cbf99b23462e3
Matthew McGrath <rubywep@hotmail.com>
2021-08-21 18:34:34 -0700

    Finish Section 20's Newsletter Signup project. (Skipped Heroku hosting section)

--- Section 20 - APIs/Newsletter-Signup/app.js
+++ Section 20 - APIs/Newsletter-Signup/app.js
@@ -3,12 +3,61 @@ const https = require('https');
 
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/signup.html');
 });
 
+app.post('/', (request, response) => {
+  const { first, last, email } = request.body;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: first,
+          LNAME: last,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const listID = 'd8d640a9f0';
+  const url = 'https://us5.api.mailchimp.com/3.0/lists/' + listID;
+
+  const options = {
+    method: 'POST',
+    auth: 'epitome87:f01c0afd95734cdb7368e8771837ca87-us5',
+  };
+
+  const mailChimpRequest = https.request(url, options, (mailchimpResponse) => {
+    if (mailchimpResponse.statusCode === 200) {
+      response.sendFile(__dirname + '/success.html');
+    } else {
+      response.sendFile(__dirname + '/failure.html');
+    }
+    mailchimpResponse.on('data', (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  mailChimpRequest.write(jsonData);
+  mailChimpRequest.end();
+});
+
+app.post('/failure', (request, response) => {
+  response.redirect('/');
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
+// API: f01c0afd95734cdb7368e8771837ca87-us5
+// List ID: d8d640a9f0
