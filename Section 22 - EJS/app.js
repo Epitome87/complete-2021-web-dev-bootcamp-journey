56afe383492b5cfea4e0196432fe6e0abeb46032
Matthew McGrath <rubywep@hotmail.com>
2021-08-28 11:19:43 -0700

    Finish and restyle Todo v1 in preparation for v2

--- Section 22 - EJS/app.js
+++ Section 22 - EJS/app.js
@@ -1,23 +1,51 @@
 const express = require('express');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
+const items = ['Buy Food', 'Cook Food', 'Eat Food'];
+const workItems = [];
+
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); // to be able to access css in list.ejs
 
-const todoList = [];
-
-app.post('/', (req, res) => {
-  const { newTodo } = req.body;
-  
-  res.render('todoList');
+// GET function for home route
+app.get('/', function (req, res) {
+  const day = date.getDate(); //date module from date.js
+  res.render('list', { listTitle: day, newListItems: items });
 });
 
-app.get('/new', (req, res) => {
-  res.render('todoForm');
+//POST function for home route
+app.post('/', function (req, res) {
+  const item = req.body.newItem;
+
+  if (req.body.list === 'Work list') {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+    res.redirect('/');
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Listening on Port 3000');
+// GET function for Work route
+app.get('/work', function (req, res) {
+  res.render('list', { listTitle: 'Work list', newListItems: workItems });
+});
+
+// POST function for home route
+app.post('/work', function (req, res) {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect('/work');
+});
+
+// GET function for About route
+app.get('/about', function (req, res) {
+  res.render('about');
+});
+
+app.listen(3000, function () {
+  console.log('Server is running on port 3000');
 });
